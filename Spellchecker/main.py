43a0bc7b53bdb0ccd1d7bbfe428df970b1b5c536
Aisha Smith <aisha.smith413@gmail.com>
2023-04-22 10:03:02 +0100

    Add files via upload

---
+++ Spellchecker/main.py
@@ -0,0 +1,36 @@
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+root = Tk()
+root.title("Spelling Checker")
+root.geometry("650x400")
+root.config(background="#609966")
+
+
+def check_spelling():
+    word = enter_text.get()
+    a = TextBlob(word)
+    right = str(a.correct())
+
+    cs = Label(root, text="Correct text is :", font=(
+        "poppins", 20), bg="#609966", fg="#000000")
+    cs.place(x=100, y=250)
+    spell.config(text=right)
+
+
+heading = Label(root, text="Spelling Checker", font=(
+    "Comic Sans MS", 20, "bold"), bg="#609966", fg="#EDF1D6")
+heading.pack(pady=(50, 0))
+enter_text = Entry(root, justify="center", width=30, font=(
+    "Comic Sans MS", 20, "bold"), bg="white", border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
+Button = Button(root, text="Check", font=("Comic Sans MS", 20,
+                "bold"), fg="#40513B", bg="#9DC08B", command=check_spelling)
+Button.pack()
+
+spell = Label(root, font=("Comic Sans MS", 20), bg="#609966", fg="#EDF1D6")
+spell.place(x=350, y=250)
+root.mainloop()
